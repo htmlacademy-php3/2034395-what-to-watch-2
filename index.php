3ac8576dd7f8759aa6deb55ade3c891f4784e630
mentalaffect <parismay.frontend@mail.ru>
2023-03-06 04:48:52 +0300

    fixes

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once "vendor/autoload.php";
-
-use Mentalaffect\WhatToWatch\repositories\MovieRepository;
-use GuzzleHttp\Client;
-
-$client = new Client();
-$movieRepository = new MovieRepository($client, $argv[1]);
