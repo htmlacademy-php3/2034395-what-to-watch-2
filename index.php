64cd2122256bb2b0ee6ef7208f63331d6e1980a2
mentalaffect <parismay.frontend@mail.ru>
2023-03-10 00:54:40 +0300

    added params check

--- index.php
+++ index.php
@@ -6,12 +6,16 @@ use Mentalaffect\WhatToWatch\repositories\MovieRepository;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 
-$client = new Client();
-$repository = new MovieRepository($client, $argv[1]);
+if (isset($argv[1], $argv[2])) {
+    $client = new Client();
+    $repository = new MovieRepository($client, $argv[1]);
 
-try {
-    $movie = $repository->getById($argv[2]);
-    fputs(STDOUT, json_encode($movie));
-} catch (GuzzleException $e) {
-    fputs(STDOUT, $e->getMessage());
+    try {
+        $movie = $repository->getById($argv[2]);
+        fputs(STDOUT, json_encode($movie));
+    } catch (GuzzleException $e) {
+        fputs(STDOUT, $e->getMessage());
+    }
+} else {
+    fputs(STDOUT, 'Error: Two parameters must be entered: (php index.php "api_key" "movie IMDB id")');
 }
