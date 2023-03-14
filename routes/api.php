b12aa9bd5d2410b8af1213462080461cbf0554ee
mentalaffect <parismay.frontend@mail.ru>
2023-03-14 22:03:57 +0300

    fixed controllers and routes

--- routes/api.php
+++ routes/api.php
@@ -29,18 +29,18 @@ Route::get('/films', [FilmsController::class, 'getAll']);
 Route::get('/films/{film}', [FilmsController::class, 'get']);
 Route::get('/films/{film}/similar', [FilmsController::class, 'similar']);
 Route::post('/films', [FilmsController::class, 'add']);
-Route::patch('/films/{filmId}', [FilmsController::class, 'change']);
+Route::patch('/films/{film}', [FilmsController::class, 'change']);
 
 Route::get('/promo', [PromoController::class, 'get']);
 
 Route::get('/favorite', [FavoriteController::class, 'get']);
-Route::post('/favorite/{filmId}/{status}', [FavoriteController::class, 'add']);
-Route::delete('/favorite/{filmId}', [FavoriteController::class, 'delete']);
+Route::post('/favorite/{film}/{status}', [FavoriteController::class, 'add']);
+Route::delete('/favorite/{film}', [FavoriteController::class, 'delete']);
 
-Route::get('/comments/{filmId}', [CommentController::class, 'get']);
-Route::post('/comments/{filmId}', [CommentController::class, 'add']);
-Route::patch('/comments/{commentId}', [CommentController::class, 'change']);
-Route::delete('/comments/{commentId}', [CommentController::class, 'delete']);
+Route::get('/comments/{film}', [CommentController::class, 'get']);
+Route::post('/comments/{film}', [CommentController::class, 'add']);
+Route::patch('/comments/{comment}', [CommentController::class, 'change']);
+Route::delete('/comments/{comment}', [CommentController::class, 'delete']);
 
 Route::get('/login', [AuthController::class, 'login']);
 Route::post('/login', [AuthController::class, 'login']);
